6c4f6a01973d239bd68a662791910d2c9c34a194
Garrett Breaux <garrett.breaux1@gmail.com>
2021-06-23 12:30:31 -0500

    initial commit

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject in school?");
+            var favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject)
+            {
+                case "English":
+                    Console.WriteLine("That's awesome, but English was my least favorite!");
+                    break;
+
+                case "Math":
+                    Console.WriteLine("I love math, I coul do equations in my sleep.");
+                    break;
+
+                case "Science":
+                    Console.WriteLine("Woohoo, was bill nye your absolute favorite?");
+                    break;
+
+                case "History":
+                    Console.WriteLine("I don't know why it's called history, should be called sleepstory.");
+                    break;
+
+                case "Athletics":
+                    Console.WriteLine("Nice, got sweat?");
+                    break;
+
+                default:
+                    Console.WriteLine("I have never heard of that subject!");
+                    break;
+            }
+        }
+    }
+}
